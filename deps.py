3d37454b29dcc0af627b784ec61a5c9f7e1c947d
= <=>
2020-11-05 19:37:54 +0300

    ChangeDeps

--- deps.py
+++ deps.py
@@ -1,9 +1,13 @@
 import os
 import sys
 
+from LogPython import LogManager; LogManager = LogManager()
+
 def run_python_tool(command):
     os.system(
         'cd \"' + os.path.dirname(sys.executable) + '\" && ' + os.path.basename(sys.executable) + ' -m ' + command)
     
 run_python_tool("pip install --upgrade pip")
 run_python_tool('pip install click bs4 requests --user')
+
+LogManager.info("Successfully installed => go to hacking :)")
